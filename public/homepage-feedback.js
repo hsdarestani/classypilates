409b7b41fdbf81a2c219ef5fce92e3a1f5c325f6
Hossein Darestani <45485005+hsdarestani@users.noreply.github.com>
2026-09-16 02:51:48 +0330

    Use current Classy WhatsApp number

--- public/homepage-feedback.js
+++ public/homepage-feedback.js
@@ -1,5 +1,5 @@
 (()=>{
-  const WHATSAPP='https://wa.me/4915253816033';
+  const WHATSAPP='https://wa.me/4951328533996';
   const whatsappIcon='<svg viewBox="0 0 24 24" aria-hidden="true"><path d="M20 11.5a8 8 0 0 1-11.8 7L4 20l1.5-4A8 8 0 1 1 20 11.5Z"/><path d="M9 8.3c.2 2 1.8 4.1 4.2 5.3.7.3 1.3.4 1.8-.2l.8-1-2.2-1.1-.6.8c-.2.2-.5.2-.8.1-1.1-.6-2-1.4-2.6-2.5-.2-.3-.1-.6.1-.8l.6-.6-.9-2.1-.4.1Z"/></svg>';
   const COACH_PHOTOS=[
     {match:name=>/^anna\s*k\b/.test(name),src:'/anna%20K.jpg',schedulePosition:'50% 52%'},
